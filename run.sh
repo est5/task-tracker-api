0e3626582d1f63c1a8db9059d3b9bde6f2239d2d
est5 <ghostrekon5500@ya.ru>
2022-08-06 13:54:29 +0300

    scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker compose -f docker/docker-compose.yml up -d
 mvn spring-boot:run clean
-docker compose -f docker/docker-compose.yml down
--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker compose -f docker/docker-compose.yml up -d
 mvn test clean
-docker compose -f docker/docker-compose.yml down
