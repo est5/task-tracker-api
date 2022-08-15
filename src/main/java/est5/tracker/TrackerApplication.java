31cefd36458cf44bce0fd492a0ff3daa396da9e4
est5 <ghostrekon5500@ya.ru>
2022-08-15 11:05:14 +0300

    temp cors

--- src/main/java/est5/tracker/TrackerApplication.java
+++ src/main/java/est5/tracker/TrackerApplication.java
@@ -18,7 +18,7 @@ public class TrackerApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
